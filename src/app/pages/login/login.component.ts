30b6634ce0b1b296390c4a8c8487798ecefcb7b0
Akshay <akshay@myvaxa.com>
2020-05-13 00:46:55 +0530

    added reservoir chart, design fix.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
+declare var $;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+  @ViewChild('sect') sect: ElementRef<HTMLElement>;
 
   ngOnInit() {
+    this.setbackgroundImageHeight();   
   }
 
+  setbackgroundImageHeight() {
+    console.log($(this.sect.nativeElement).offset().top);
+    let height = window.innerHeight - $(this.sect.nativeElement).offset().top;
+    this.sect.nativeElement.style.height = height + 'px';
+    console.log('height ', height)
+  }
 }
